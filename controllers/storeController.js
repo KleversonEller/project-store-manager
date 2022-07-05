7fe01c213f3881e0a73f3ec82f8dc62bdffcc143
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-04 22:40:14 -0300

    :sparkles: feat: Criando o Controller newProduct
    Criando a função que faz os tratamentos de response e request da inserção de um novo produto no banco

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,7 +19,21 @@ const findForId = rescue(async (req, res, next) => {
   return res.status(200).json(result);
 });
 
+const newProduct = rescue(async (req, res, next) => {
+  const { error } = Joi.object({
+    name: Joi.string().required().not().empty().min(5),
+  }).validate(req.body);
+  if (error) return next(error);
+
+  const { name } = req.body;
+  const result = await storeService.newProduct(name);
+  if (!result) return next(error);
+
+  return res.status(201).json(result);
+});
+
 module.exports = {
   getAll,
   findForId,
+  newProduct,
 };
