fd06db047567a1f2fc4e4e728263ba1374463972
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-05 19:37:04 -0300

    :lipstick: style: Corrigindo formatação
    Corrigindo a formatação do codigo para atender as regras de lint

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,24 +1,21 @@
+const statusByErrorCode = {
+  'any.required': 400,
+  'string.min': 422,
+  'number.positive': 422,
+  'number.min': 422,
+  notFound: 404,
+  invalidData: 400,
+  alreadyExists: 409,
+};
 module.exports = (err, _req, res, _next) => {
-  const statusByErrorCode = {
-    'any.required': 400,
-    'string.min': 422,
-    notFound: 404,
-    invalidData: 400,
-    alreadyExists: 409,
-  };
-
   if (err.isJoi) {
     const status = statusByErrorCode[err.details[0].type] || 500;
-
     return res.status(status).json({ message: err.details[0].message });
   }
-
   if (err.code) {
   const status = statusByErrorCode[err.code] || 500;
-
   return res.status(status).json(err);
-  };
-
+  }
   console.error(err);
   return res.status(500).json({ message: 'Internal server error' });
 };
