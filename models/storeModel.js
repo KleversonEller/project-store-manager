c903a2ef7648291db20cdb64da452ec1825f9bde
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-05 19:26:08 -0300

    :sparkles: feat: Criando o Model newSales
    Criando a função que insere uma nova venda nas tabelas 'sales' e 'sales_product'

--- models/storeModel.js
+++ models/storeModel.js
@@ -8,7 +8,7 @@ const getAll = async () => {
 
   const [result] = await connection.execute(query);
 
-  return result.length < 0 ? null : result;
+  return result.length ? result : null;
 };
 
 const findForId = async (id) => {
@@ -16,7 +16,7 @@ const findForId = async (id) => {
 
   const [result] = await connection.execute(query, [id]);
 
-  return result.length < 0 ? null : result[0];
+  return result.length ? result[0] : null;
 };
 
 const newProduct = async (nameProduct) => {
@@ -27,8 +27,27 @@ const newProduct = async (nameProduct) => {
   return { id: result.insertId, name: nameProduct };
 };
 
+const newSales = async (arraySales) => {
+  const querySales = `INSERT INTO ${DATABASE}.sales (date) VALUES (NOW ());`;
+  const idSale = await connection.execute(querySales).then(([id]) => id.insertId);
+
+  const querySalesProduct = `
+  INSERT INTO ${DATABASE}.sales_products (sale_id, product_id, quantity) VALUES (?,?,?);`;
+
+  arraySales.forEach(async (objeto) => {
+    await connection
+      .execute(querySalesProduct, [idSale, objeto.productId, objeto.quantity]);
+  });
+
+  return {
+    id: idSale,
+    itemsSold: arraySales,
+  };
+};
+
 module.exports = {
   getAll,
   findForId,
   newProduct,
+  newSales,
 };
