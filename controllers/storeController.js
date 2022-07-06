3c972177a4a3bfbdd0ef6c7dc4dc033cc6796237
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-05 23:54:42 -0300

    :bug: fix: Possivel bug com o avaliador
    Removendo o rescue para possivel correção de erro no avaliador remoto e para efetuar os testes unitarios da camada de controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,25 +1,24 @@
 const Joi = require('joi');
-const rescue = require('express-rescue');
 const storeService = require('../services/storeService');
 
-const getAll = rescue(async (_req, res, next) => {
+const getAll = async (_req, res, next) => {
   const result = await storeService.getAll();
 
   if (!result) return next(result);
 
   return res.status(200).json(result);
-});
+};
 
-const findForId = rescue(async (req, res, next) => {
+const findForId = async (req, res, next) => {
   const { id } = req.params;
   const result = await storeService.findForId(id);
 
   if (result.error) return next(result.error);
 
   return res.status(200).json(result);
-});
+};
 
-const newProduct = rescue(async (req, res, next) => {
+const newProduct = async (req, res, next) => {
   const { error } = Joi.object({
     name: Joi.string().required().min(5).not(),
   }).validate(req.body);
@@ -30,14 +29,14 @@ const newProduct = rescue(async (req, res, next) => {
   if (!result) return next(error);
 
   return res.status(201).json(result);
-});
+};
 
-const newSales = rescue(async (req, res, next) => {
+const newSales = async (req, res, next) => {
   const result = await storeService.newSales(req.body);
 
   if (result[0].error) return next(result[0].error);
   return res.status(201).json(result[0]);
-});
+};
 
 module.exports = {
   getAll,
