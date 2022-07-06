7ee9fc30a168d7099165dc2e5c811c8b4b050d68
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-06 16:57:34 -0300

    :sparkles: feat: Criando o Controller updateProduct
    Criando a função que atualiza um produto no banco

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -38,9 +38,25 @@ const newSales = async (req, res, next) => {
   return res.status(201).json(result[0]);
 };
 
+const updateProduct = async (req, res, next) => {
+  const { error } = Joi.object({
+    name: Joi.string().required().min(5).not(),
+  }).validate(req.body);
+  if (error) return next(error);
+
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const result = await storeService.updateProduct(id, name);
+  if (result.error) return next(result.error);
+
+  return res.status(200).json(result);
+};
+
 module.exports = {
   getAll,
   findForId,
   newProduct,
   newSales,
+  updateProduct,
 };
